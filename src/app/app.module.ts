a7a415c12469f0eacc8ad3e23dba0b4bbecffad4
Cleiber Ziviani <cleiberz@yahoo.com.br>
2021-04-20 19:59:27 -0300

    setup inicial do store-devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,8 @@ import { StoreModule } from '@ngrx/store';
     AppRoutingModule,
     HomeModule,
     BookmarksModule,
-    StoreModule.forRoot({})
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
