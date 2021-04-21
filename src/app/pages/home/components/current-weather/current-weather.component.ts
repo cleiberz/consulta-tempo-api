2fa251bcfc15b6ea4b65aec62e0fe48113989ad1
Cleiber Ziviani <cleiberz@yahoo.com.br>
2021-04-21 13:44:29 -0300

    projeto concluído

--- src/app/pages/home/components/current-weather/current-weather.component.ts
+++ src/app/pages/home/components/current-weather/current-weather.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, Output, ChangeDetectionStrategy, EventEmitter } from '@angular/core';
 
 import { CityWeather } from '../../../../shared/models/weather.model';
+import { Units } from 'src/app/shared/models/units.enum';
 
 @Component({
   selector: 'jv-current-weather',
@@ -12,10 +13,11 @@ export class CurrentWeatherComponent {
 
   @Input() cityWeather: CityWeather;
   @Input() isFavorite: boolean;
+  @Input() unit: Units;
   @Output() toggleBookmark = new EventEmitter();
 
   get cityName(): string {
-    return `${this.cityWeather.city.name}, ${this.cityWeather.city.country}`;
+    return `${this.cityWeather.city.name}, ${this.cityWeather.city.country}`; 
   }
 
   onToggleBookmark() {
--- src/app/pages/home/state/home.effects.ts
+++ src/app/pages/home/state/home.effects.ts
@@ -5,6 +5,7 @@ import { map, mergeMap, catchError } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 
 import { WeatherService } from 'src/app/shared/services/weather.service';
+import { CityWeather } from 'src/app/shared/models/weather.model';
 import * as fromHomeActions from './home.actions';
 
 @Injectable()
@@ -18,13 +19,26 @@ export class HomeEffects {
         this.store.dispatch(fromHomeActions.loadCurrentWeatherFailed());
         return caught$;
       }),
-      map((entity: any) => fromHomeActions.loadCurrentWeatherSuccess({ entity })),
+      map((entity: CityWeather) => fromHomeActions.loadCurrentWeatherSuccess({ entity })),
     ),
+  );
+
+  loadCurrentWeatherById$ = createEffect(() => this.actions$
+    .pipe(
+      ofType(fromHomeActions.loadCurrentWeatherById),
+      mergeMap(({ id }: { id: string }) =>
+        this.weatherService.getCityWeatherById(id)
+      ),
+      catchError((err, caught$) => {
+        this.store.dispatch(fromHomeActions.loadCurrentWeatherFailed());
+        return caught$;
+      }),
+      map((entity: CityWeather) => fromHomeActions.loadCurrentWeatherSuccess({entity})),
+    )
   );
 
   constructor(private actions$: Actions,
               private store: Store,
-              private weatherService: WeatherService)
-       {
+              private weatherService: WeatherService) {
   }
 }
